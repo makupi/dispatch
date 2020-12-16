b7613afb4211a8b74deba351e9bfaef989883bf4
makubob <makupi@icloud.com>
2020-12-17 00:28:54 +0100

    Add Context.ask

--- utils/context.py
+++ utils/context.py
@@ -1,5 +1,59 @@
+import asyncio
+import inspect
+from typing import Union, Any, Optional, Callable
+
+import discord
 from discord.ext import commands
 
 
 class Context(commands.Context):
-    pass
+    async def ask(
+        self,
+        question: str,
+        parser: Optional[Union[Callable[[str], Any]]] = None,
+        use_embed: bool = True,
+        embed_color: Optional[discord.Color] = None,
+        delete_question: bool = True,
+        delete_answer: bool = True,
+    ) -> Union[Optional[str], Any]:
+        """
+        Ask the user a question, returns the response as string or whatever parser returns
+        :param question: Question as string
+        :param parser: Optional. A callable or coroutine that gets the answer as a string ask will return
+        :param use_embed: Defaults to True. Set to False to disable embeds
+        :param embed_color: Optional. Custom color for the embed
+        :param delete_question: Defaults to True. Delete the question before returning
+        :param delete_answer: Defaults to True. Delete the answer before returning
+        :return: Answer as string or whatever parser returns
+        """
+        if use_embed:
+            embed = discord.Embed(description=question, colour=embed_color)
+            question = await self.send(embed=embed)
+        else:
+            question = await self.send(question)
+
+        def check(m: discord.Message) -> bool:
+            return m.author == self.author and m.channel == self.channel
+
+        message = None
+        try:
+            message = await self.bot.wait_for("message", check=check)
+        except asyncio.TimeoutError:
+            pass
+
+        if delete_question:
+            await question.delete()
+        if message is None:
+            return None
+
+        reply = message.content
+        if parser is not None:
+            if inspect.isfunction(parser):
+                reply = parser(message.content)
+            elif inspect.iscoroutine(parser):
+                reply = await parser(message.content)
+
+        if delete_answer:
+            await message.delete()
+
+        return reply
