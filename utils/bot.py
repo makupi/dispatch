be4dfd95427154a90e2b9e9fefadb5c6b937f414
makubob <makupi@icloud.com>
2020-12-21 14:27:07 +0100

    Modify BotBase to avoid duplicate code

--- utils/bot.py
+++ utils/bot.py
@@ -1,13 +1,16 @@
 import discord
-from discord.ext import commands
+from discord.ext.commands.bot import BotBase
 from utils.context import Context
 
 
-class Bot(commands.Bot):
+class CustomBotBase(BotBase):
     async def get_context(self, message: discord.Message, *, cls=Context):
         return await super().get_context(message, cls=cls)
 
 
-class AutoShardedBot(commands.AutoShardedBot):
-    async def get_context(self, message: discord.Message, *, cls=Context):
-        return await super().get_context(message, cls=cls)
+class Bot(CustomBotBase, discord.Client):
+    pass
+
+
+class AutoShardedBot(CustomBotBase, discord.AutoShardedClient):
+    pass
